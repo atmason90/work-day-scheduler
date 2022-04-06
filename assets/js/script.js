a097922090aa7f3cf72758857a6152cb2f1c2082
atmason90 <atmason90@gmail.com>
2022-04-05 21:40:13 -0700

    work on pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 // What do I need?
-    // moment.js to display current day in p tag with class .currentDay
-    
+    // var using moment.js to display current day in the p tag with class .currentDay
+    // var using moment.js for current time of day
+
+    // variables for:
+        // hours of day the scheduler will span (8am-6pm)
+        // timeblock
+            // foreach to create a separate timeblock for every hour
+        
