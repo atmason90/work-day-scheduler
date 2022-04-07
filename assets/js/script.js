b342b8bbf82c29dc60267f4197f5dd578f7d9fa3
atmason90 <atmason90@gmail.com>
2022-04-06 18:08:06 -0700

    add time using moment js and set interval for constant updating of time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
 // What do I need?
     // var using moment.js to display current day in the p tag with class .currentDay
     // var using moment.js for current time of day
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss"));
+
+// set interval to constantly update the time
+let updateTime = setInterval(function () {
+    let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss")
+    $("#currentDay").text(currentTime)
+}, 1000);
+
 
     // variables for:
         // hours of day the scheduler will span (8am-6pm)
         // timeblock
             // foreach to create a separate timeblock for every hour
+
+// Extra small devices (portrait phones, less than 576px)
+// No media query for `xs` since this is the default in Bootstrap
+
+// // Small devices (landscape phones, 576px and up)
+// @media (min-width: 576px) { ... }
+
+// // Medium devices (tablets, 768px and up)
+// @media (min-width: 768px) { ... }
+
+// // Large devices (desktops, 992px and up)
+// @media (min-width: 992px) { ... }
+
+// // Extra large devices (large desktops, 1200px and up)
+// @media (min-width: 1200px) { ... }
         
